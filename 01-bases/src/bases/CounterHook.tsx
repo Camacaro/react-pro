67e30bf9ec52a6ea44de217859ab2c0350df8a34
Jesus Camacaro <jesus.alejandr@gmail.com>
2022-03-02 12:43:12 -0300

    hook

---
+++ 01-bases/src/bases/CounterHook.tsx
@@ -0,0 +1,14 @@
+import { useConter } from "../hooks/useCounter";
+
+export const CounterHook = () => {
+  const {counter, elementToAnimate, handleClick} = useConter({ maxCount: 15 });
+
+  return (
+    <>
+      <h1>CounterHook: </h1>
+      <h2 ref={elementToAnimate}>{counter} </h2>
+
+      <button onClick={handleClick}> +1 </button>
+    </>
+  )
+}
